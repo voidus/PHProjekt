72a3d0b6e9f68071e30e10a6f3c85d1aca5932da
David Soria Parra <david.soria_parra@mayflower.de>
2012-10-20 20:40:59 +0200

    PHPROJEKT-424: Implement a contact specific save method
    Contact don't have any item or role rights. The default save method
    requires rights for an item. Therfeore we need a seaprate, contact
    specific, save method. We split the current save logic into a separate
    function that can be overwritten. This save method is used in both
    jsonSaveMultiple nad jsonSave.

--- phprojekt/application/Contact/Controllers/IndexController.php
+++ phprojekt/application/Contact/Controllers/IndexController.php
@@ -18,4 +18,47 @@
  */
 class Contact_IndexController extends IndexController
 {
+    /**
+     * Saves a contact item
+     *
+     * Contacts, unlike other items, don't have a role or item rights.
+     * The save method doesn't save any rights whatsoever.
+     */
+    protected function _saveModel(Phprojekt_Model_Interface $model, $params, $newItem)
+    {
+        $model = Default_Helpers_Save::parameterToModel($model, $params, $newItem);
+        /* contacts are always saved under the root project */
+        $model->projectId = 1;
+        $model->save();
+    }
+
+    public function jsonSaveMultipleAction()
+    {
+        $data    = (array) $this->getRequest()->getParam('data');
+        $showId  = array();
+        $model   = $this->getModelObject();
+        $success = true;
+        $this->setCurrentProjectId();
+
+        foreach ($data as $id => $fields) {
+            try {
+                $model->find((int) $id);
+                $model = Default_Helpers_Save::parameterToModel($model, $fields, false);
+                $model->save();
+                $showId[] = $id;
+            } catch (Zend_Controller_Action_Exception $error) {
+                $message = sprintf("ID %d. %s", $id, $error->getMessage());
+                $success = false;
+                $showId  = array($id);
+                break;
+            }
+        }
+
+        if ($success) {
+            $message    = Phprojekt::getInstance()->translate(self::EDIT_MULTIPLE_TRUE_TEXT);
+            $resultType = 'success';
+        } else {
+            $resultType = 'error';
+        }
+    }
 }
